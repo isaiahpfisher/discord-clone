e5cd2a236c1e864a16d66bd6969b7ccba02a27e5
isaiahpfisher <isaiahpfisher@gmail.com>
2023-12-29 11:19:19 -0600

    navigation sidebar

---
+++ components/navigation/NavigationSidebar.tsx
@@ -0,0 +1,56 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { NavigationAction } from "./NavigationAction";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./NavigationItem";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { UserButton } from "@clerk/nextjs";
+
+export const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
+      <NavigationAction />
+      <Separator className="mx-auto h-[2px] w-10 rounded-md bg-zinc-200 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="mt-auto flex flex-col items-center gap-y-4 pb-3">
+        <ThemeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "size-[48px]",
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+};
